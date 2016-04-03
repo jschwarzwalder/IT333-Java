eebcefb36b731ded08429291f49044e51a65ae0a
Jami <jamischwarzwalder@gmail.com>
2016-04-02 19:21:02 -0700

    My first Java in Eclipse

---
+++ HelloWorld/src/net/greenrivertech/jschwarzwalder/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.greenrivertech.jschwarzwalder;
+
+/**
+ * @author Jami Schwarzwalder
+ *
+ */
+public abstract class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// print out a string
+		System.out.println("Hello world!");
+	}
+
+}
