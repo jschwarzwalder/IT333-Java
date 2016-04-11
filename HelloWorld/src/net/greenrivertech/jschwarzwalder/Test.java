dd0374a11f6068a3eba728f0805cbc167c6ad34a
Jami <jamischwarzwalder@gmail.com>
2016-04-10 18:12:23 -0700

    Trying to apply a algorithm described in book.

---
+++ HelloWorld/src/net/greenrivertech/jschwarzwalder/Test.java
@@ -0,0 +1,21 @@
+package net.greenrivertech.jschwarzwalder;
+
+import java.util.ArrayList;
+
+public class Test {
+	public static void main(String[] args) {
+		ArrayList interim = new ArrayList;
+		int check = 0;
+		int[] numbers = [0,1,2,3,4,5,6];
+		int k;
+		
+		for(int i = 1; i <= k; i++){
+	        if(check == numbers[i] && check != interim.get[interim.size() - 1]){
+	                interim.add(check);
+	        }else{
+	            check = numbers[i];
+	        }
+	    }
+	}
+
+}
