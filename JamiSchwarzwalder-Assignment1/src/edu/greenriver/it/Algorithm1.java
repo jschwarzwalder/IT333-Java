a023685de68daa0c96c988e827aeb797632b5678
Jami <jamischwarzwalder@gmail.com>
2016-04-10 18:11:59 -0700

    Assignment 1 creating first class

---
+++ JamiSchwarzwalder-Assignment1/src/edu/greenriver/it/Algorithm1.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package edu.greenriver.it;
+
+import java.util.Random;
+
+/*
+ * Jami Schwarzwalder
+ * 4/9/2016
+ * Algorithm1.java
+ * Assignment #1 - Algorithm Analysis
+ * 
+ * Fill the array a from a[0] to a[n-1] as follows: To fill a[i], 
+ * generate random numbers until you get one that is not already in a[0], a[1], … a[i-1].
+ */
+
+/**
+ * @author Jami Schwarzwalder
+ * @version 1.0
+ */
+
+public class Algorithm1 {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+     * Returns random permutation of the first N integers
+     *
+     * @param lengthOfArray length of array to be returned 
+     * @returns an array of integers the length lengthOfArray
+     *  			ordered randomly
+     */
+	public int[] randomInts (int lengthOfArray){
+		//Create array to store integers
+		int[] randomInts = new int[lengthOfArray];
+		Random randomInt = new Random ();
+		//for each element in the array, generate a random integer
+		for (int i = 0; i < lengthOfArray-1; ){
+			int number = randomInt.nextInt(lengthOfArray);
+			//if int is already in the array then generate a new int
+			
+		}
+		
+		return randomInt;
+	}
+	
+	/**
+     * Searches given array to see if value is inside.
+     *
+     * @param array an array of numbers 
+     * @param value a number 
+     * @returns  whether value is in array
+     */
+	private boolean inArray(int[] array, int value){
+		//search array, if value in array return true
+		for (int i = 0; i< array.length; i++){
+			if (value == array[i] ){
+				return true;
+			}
+		}
+		
+		//if not in array return false
+		return false;
+	}
+
+}
