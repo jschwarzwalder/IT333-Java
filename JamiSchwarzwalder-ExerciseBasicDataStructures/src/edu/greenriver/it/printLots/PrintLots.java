f808f532b50809881c50689c6f65f0a1ac16fcaf
Jami <jamischwarzwalder@gmail.com>
2016-04-14 23:53:22 -0700

    adding Java docs

--- JamiSchwarzwalder-ExerciseBasicDataStructures/src/edu/greenriver/it/printLots/PrintLots.java
+++ JamiSchwarzwalder-ExerciseBasicDataStructures/src/edu/greenriver/it/printLots/PrintLots.java
@@ -1,10 +1,24 @@
 package edu.greenriver.it.printLots;
 
+/*
+ * Jami Schwarzwalder
+ * 4/14/2016
+ * PrintLots.java
+ * Exercises - Basic Data Structures
+ * 
+ */
 public class PrintLots {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	// You are given a list, L, and another list, P, containing integers sorted in ascending order. 
+	// The operation printLots(L, P) will print the elements in L that are in positions specified by P. 
+	// For instance, if P = 1, 3, 4, 6, the elements in positions 1, 2, 4, and 6 in L are printed.
+	
+	public static void printLots(List L, List P){
+		
 	}
 
 }
