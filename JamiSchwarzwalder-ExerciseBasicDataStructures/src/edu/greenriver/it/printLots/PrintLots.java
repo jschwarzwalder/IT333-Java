66146b86db74324410f02bbbeaf90a53fec8d401
Jami <jamischwarzwalder@gmail.com>
2016-04-16 13:26:46 -0700

    adding tests

--- JamiSchwarzwalder-ExerciseBasicDataStructures/src/edu/greenriver/it/printLots/PrintLots.java
+++ JamiSchwarzwalder-ExerciseBasicDataStructures/src/edu/greenriver/it/printLots/PrintLots.java
@@ -1,7 +1,9 @@
 package edu.greenriver.it.printLots;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 /*
  * Jami Schwarzwalder
@@ -23,16 +25,61 @@ import java.util.Iterator;
  */
 public class PrintLots {
 
-	public static <E> void main(String[] args) {
+	public static void main(String[] args) {
 		// Testing code
 		
-		Collection<E> Inputs = new Collection(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		ArrayList<Integer> ArrayInputs = new ArrayList<Integer>();
+		LinkedList<Integer> LinkedInputs = new LinkedList<Integer>();
+		for (int i=1; i < 10; i++){
+			LinkedInputs.add(i);
+		}
 		
-		List<E> Test1 = new List(1,2,3);
-		List<E> Test2 = new List(7,8,9);
+		ArrayList<Integer> Test1 = new ArrayList<Integer> (3);
+		Test1.add(1);
+		Test1.add(2);
+		Test1.add(3);
+		ArrayList<Integer> Test2 = new ArrayList<Integer> (3);
+		Test2.add(7);
+		Test2.add(8);
+		Test2.add(9);
+		ArrayList<Integer> Test3 = new ArrayList<Integer> ();
+		LinkedList<Integer> Test4 = new LinkedList<Integer>();
+		Test4.add(1);
+		Test4.add(2);
+		Test4.add(3);
+		LinkedList<Integer> Test5 = new LinkedList<Integer>();
+		Test5.add(7);
+		Test5.add(8);
+		Test5.add(9);
+		LinkedList<Integer> Test6 = new LinkedList<Integer> ();
+		for (int i=1; i < 10; i++){
+			Test6.add(i);
+		}
 
-		printLots (Inputs, Test1);
-		printLots (Inputs, Test2);
+		System.out.println("ArrayList Test1: ");
+		printLots (ArrayInputs, Test1);
+		System.out.println("ArrayList Test2: ");
+		printLots (ArrayInputs, Test2);
+		System.out.println("ArrayList Test3: ");
+		printLots (ArrayInputs, Test3);
+		System.out.println("ArrayList Test4: ");
+		printLots (ArrayInputs, Test4);
+		System.out.println("ArrayList Test5: ");
+		printLots (ArrayInputs, Test5);
+		System.out.println("ArrayList Test6: ");
+		printLots (ArrayInputs, Test6);
+		System.out.println("LinkedList Test1: ");
+		printLots (LinkedInputs, Test1);
+		System.out.println("LinkedList Test2: ");
+		printLots (LinkedInputs, Test2);
+		System.out.println("LinkedList Test3: ");
+		printLots (LinkedInputs, Test3);
+		System.out.println("LinkedList Test4: ");
+		printLots (LinkedInputs, Test4);
+		System.out.println("LinkedList Test5: ");
+		printLots (LinkedInputs, Test5);
+		System.out.println("LinkedList Test6: ");
+		printLots (LinkedInputs, Test6);
 	}
 	
 	/**
@@ -55,10 +102,11 @@ public class PrintLots {
 			if (inputListIterator.hasNext()){
 				Object currentValue = inputListIterator.next();
 				if (positions.contains(currentValue)){
-					System.out.println(currentValue);
+					System.out.print(currentValue);
 				}
 			}
 		}
+		System.out.println();
 			
 			
 	}
