a20253ac597da1f4a902c8e4a413419508215195
Jami <jamischwarzwalder@gmail.com>
2016-04-16 14:32:28 -0700

    Added information about BigOh

--- JamiSchwarzwalder-ExerciseBasicDataStructures/src/edu/greenriver/it/printLots/PrintLots.java
+++ JamiSchwarzwalder-ExerciseBasicDataStructures/src/edu/greenriver/it/printLots/PrintLots.java
@@ -2,7 +2,6 @@ package edu.greenriver.it.printLots;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.LinkedList;
 
 /*
@@ -13,6 +12,8 @@ import java.util.LinkedList;
  *  You are given a list, L, and another list, P, containing integers sorted in ascending order. 
  *  The operation printLots(L, P) will print the elements in L that are in positions specified by P. 
  *  For instance, if P = 1, 3, 4, 6, the elements in positions 1, 2, 4, and 6 in L are printed.
+ *  
+ *  1b) Running time of the procedure is Length of L * Length of P ie Omega n and BigOh n^2
  */
 
 /**
@@ -28,58 +29,64 @@ public class PrintLots {
 	public static void main(String[] args) {
 		// Testing code
 		
-		ArrayList<Integer> ArrayInputs = new ArrayList<Integer>();
-		LinkedList<Integer> LinkedInputs = new LinkedList<Integer>();
+		ArrayList<Integer> arrayInputs = new ArrayList<Integer>();
 		for (int i=1; i < 10; i++){
-			LinkedInputs.add(i);
+			arrayInputs.add(i);
+		}
+		LinkedList<Integer> linkedInputs = new LinkedList<Integer>();
+		for (int i=1; i < 10; i++){
+			linkedInputs.add(i);
 		}
 		
-		ArrayList<Integer> Test1 = new ArrayList<Integer> (3);
-		Test1.add(1);
-		Test1.add(2);
-		Test1.add(3);
-		ArrayList<Integer> Test2 = new ArrayList<Integer> (3);
-		Test2.add(7);
-		Test2.add(8);
-		Test2.add(9);
-		ArrayList<Integer> Test3 = new ArrayList<Integer> ();
-		LinkedList<Integer> Test4 = new LinkedList<Integer>();
-		Test4.add(1);
-		Test4.add(2);
-		Test4.add(3);
-		LinkedList<Integer> Test5 = new LinkedList<Integer>();
-		Test5.add(7);
-		Test5.add(8);
-		Test5.add(9);
-		LinkedList<Integer> Test6 = new LinkedList<Integer> ();
+		ArrayList<Integer> test1 = new ArrayList<Integer> (3);
+		test1.add(1);
+		test1.add(3);
+		test1.add(5);
+		ArrayList<Integer> test2 = new ArrayList<Integer> (3);
+		test2.add(7);
+		test2.add(8);
+		test2.add(9);
+		ArrayList<Integer> test3 = new ArrayList<Integer> ();
 		for (int i=1; i < 10; i++){
-			Test6.add(i);
+			test3.add(i);
+		}
+		LinkedList<Integer> test4 = new LinkedList<Integer>();
+		test4.add(1);
+		test4.add(3);
+		test4.add(5);
+		LinkedList<Integer> test5 = new LinkedList<Integer>();
+		test5.add(7);
+		test5.add(8);
+		test5.add(9);
+		LinkedList<Integer> test6 = new LinkedList<Integer> ();
+		for (int i=1; i < 10; i++){
+			test6.add(i);
 		}
 
 		System.out.println("ArrayList Test1: ");
-		printLots (ArrayInputs, Test1);
+		printLots (arrayInputs, test1);
 		System.out.println("ArrayList Test2: ");
-		printLots (ArrayInputs, Test2);
+		printLots (arrayInputs, test2);
 		System.out.println("ArrayList Test3: ");
-		printLots (ArrayInputs, Test3);
+		printLots (arrayInputs, test3);
 		System.out.println("ArrayList Test4: ");
-		printLots (ArrayInputs, Test4);
+		printLots (arrayInputs, test4);
 		System.out.println("ArrayList Test5: ");
-		printLots (ArrayInputs, Test5);
+		printLots (arrayInputs, test5);
 		System.out.println("ArrayList Test6: ");
-		printLots (ArrayInputs, Test6);
+		printLots (arrayInputs, test6);
 		System.out.println("LinkedList Test1: ");
-		printLots (LinkedInputs, Test1);
+		printLots (linkedInputs, test1);
 		System.out.println("LinkedList Test2: ");
-		printLots (LinkedInputs, Test2);
+		printLots (linkedInputs, test2);
 		System.out.println("LinkedList Test3: ");
-		printLots (LinkedInputs, Test3);
+		printLots (linkedInputs, test3);
 		System.out.println("LinkedList Test4: ");
-		printLots (LinkedInputs, Test4);
+		printLots (linkedInputs, test4);
 		System.out.println("LinkedList Test5: ");
-		printLots (LinkedInputs, Test5);
+		printLots (linkedInputs, test5);
 		System.out.println("LinkedList Test6: ");
-		printLots (LinkedInputs, Test6);
+		printLots (linkedInputs, test6);
 	}
 	
 	/**
@@ -92,21 +99,22 @@ public class PrintLots {
      * @return none
      * 
      */
-	
+	//1b O(L.length*P.length) 
 	public static <E> void printLots( Collection<E> inputList,  Collection<E> positions){
 		if ((!positions.isEmpty()) && (!inputList.isEmpty())){
 			//create a count for the current position
 			int indexOf = 0;
 			//create iterator for the input List
-			for (E currentValue: inputList)
+			for (E currentValue: inputList){
 				//go through each element in list and see if it is contained in position.
 				//if is print
 				indexOf ++;
 				
 				if (positions.contains(indexOf)){
 					System.out.print(currentValue);
+					System.out.print(" ");
 				}
-				
+			}
 		}
 		System.out.println();
 	}		
