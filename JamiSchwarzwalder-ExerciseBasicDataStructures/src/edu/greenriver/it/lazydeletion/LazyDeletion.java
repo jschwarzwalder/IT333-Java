301186ed6b5bf4e0ca087dd47ee58e4a37fb2ac0
Jami <jamischwarzwalder@gmail.com>
2016-04-16 19:34:42 -0700

    adding Java Docs

--- JamiSchwarzwalder-ExerciseBasicDataStructures/src/edu/greenriver/it/lazydeletion/LazyDeletion.java
+++ JamiSchwarzwalder-ExerciseBasicDataStructures/src/edu/greenriver/it/lazydeletion/LazyDeletion.java
@@ -1,9 +1,33 @@
 package edu.greenriver.it.lazydeletion;
 
+/*
+ * Jami Schwarzwalder
+ * 4/15/2016
+ * LazyDeletion.java
+ * Exercises - Basic Data Structures
+ * 
+ * An alternative to the deletion strategy we have given is to use lazy deletion. 
+ * To delete an element, we merely mark it deleted (using a boolean field). 
+ * The number of deleted and non-deleted elements in the list is kept as part of the data structure. 
+ * If there are as many deleted elements as non-deleted elements, we traverse the entire list, 
+ * performing the standard deletion algorithm on all marked nodes.
+ * 
+ * List the advantages and disadvantages of lazy deletion.
+ * 
+ * Write routines to implement the standard linked list operations using lazy deletion. 
+ * As a minimum you should write the add(x), remove(x), size(), isEmpty() and contains(x) methods.
+ */
+
+/**
+ * Use Lazy Deletion on a standard linked list
+ * 
+ * @author Jami Schwarzwalder
+ * @version 1.0
+ */
 public class LazyDeletion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
