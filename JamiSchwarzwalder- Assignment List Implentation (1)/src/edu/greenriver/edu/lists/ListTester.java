1fea2395213140ffd053e30492b28fad79757f34
Jami <jamischwarzwalder@gmail.com>
2016-04-17 03:18:39 -0700

    Tester skeleton for Assignment List

---
+++ JamiSchwarzwalder- Assignment List Implentation (1)/src/edu/greenriver/edu/lists/ListTester.java
@@ -0,0 +1,75 @@
+package edu.greenriver.edu.lists;
+
+public class ListTester
+{
+	public static void main(String[] args)
+	{
+		//run the tests
+		testAdd();
+		testInsert();
+		testIsEmpty();
+		testSize();
+		testClear();
+		testIndexOf();
+		testContains();
+		testGet();
+		testSet();
+		testRemoveElement();
+		testRemoveIndex();
+	}
+	
+	public static void testAdd()
+	{
+		//this method tests list.add(element)
+	}
+	
+	public static void testInsert()
+	{
+		//this method tests list.add(index, element)
+	}
+	
+	public static void testIsEmpty()
+	{
+		//this method tests list.isEmpty()
+	}
+	
+	public static void testSize()
+	{
+		//this method tests list.size()
+	}
+	
+	public static void testClear()
+	{
+		//this method tests list.clear()
+	}
+	
+	public static void testIndexOf()
+	{
+		//this method tests list.indexOf(element)
+	}
+	
+	public static void testContains()
+	{
+		//this method tests list.contains(element)
+	}
+	
+	public static void testGet()
+	{
+		//this method tests list.get(index)
+	}
+	
+	public static void testSet()
+	{
+		//this method tests list.set(index, element)
+	}
+	
+	public static void testRemoveElement()
+	{
+		//this method tests list.remove(element)
+	}
+	
+	public static void testRemoveIndex()
+	{
+		//this method tests list.remove(index)
+	}
+}
