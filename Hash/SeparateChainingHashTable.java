c3494d8a553275d5ceca04e83d1145eec4c438a0
Jami <jamischwarzwalder@gmail.com>
2016-04-21 23:25:52 -0700

    adding methods for building, adding and removing elements

--- Hash/SeparateChainingHashTable.java
+++ Hash/SeparateChainingHashTable.java
@@ -1,18 +1,75 @@
 public class SeparateChainingHashTable<AnyType>
  {
- public SeparateChainingHashTable( )
- { /* Figure 5.9 */ }
- public SeparateChainingHashTable( int size )
- { /* Figure 5.9 */ }
+	/**
+	 * Construct the hash table.
+	 */
+	 public SeparateChainingHashTable( )
+	 {
+	 this( DEFAULT_TABLE_SIZE );
+	 }
 
- public void insert( AnyType x )
- { /* Figure 5.10 */ }
- public void remove( AnyType x )
- { /* Figure 5.10 */ }
+	 /**
+	 * Construct the hash table.
+	 * @param size approximate table size.
+	 */
+	 public SeparateChainingHashTable( int size )
+	 {
+	 theLists = new LinkedList[ nextPrime( size ) ];
+	 for( int i = 0; i < theLists.length; i++ )
+	 theLists[ i ] = new LinkedList<>( );
+	 }
+
+
+ /**
+ * Find an item in the hash table.
+ * @param x the item to search for.
+ * @return true if x is not found.
+ */
  public boolean contains( AnyType x )
- { /* Figure 5.10 */ }
+ {
+ List<AnyType> whichList = theLists[ myhash( x ) ];
+ return whichList.contains( x );
+ }
+
+ /**
+ * Insert into the hash table. If the item is
+ * already present, then do nothing.
+ * @param x the item to insert.
+ */
+ public void insert( AnyType x )
+ {
+ List<AnyType> whichList = theLists[ myhash( x ) ];
+ if( !whichList.contains( x ) )
+ {
+ whichList.add( x );
+
+ // Rehash; see Section 5.5
+ if( ++currentSize > theLists.length )
+ rehash( );
+ }
+ }
+
+ /**
+ * Remove from the hash table.
+ * @param x the item to remove.
+ */
+ public void remove( AnyType x )
+ {
+ List<AnyType> whichList = theLists[ myhash( x ) ];
+ if( whichList.contains( x ) )
+ {
+ whichList.remove( x );
+ currentSize--;
+ }
+ /**
+ * Make the hash table logically empty.
+ */
  public void makeEmpty( )
- { /* Figure 5.9 */ }
+ {
+ for( int i = 0; i < theLists.length; i++ )
+ theLists[ i ].clear( );
+ currentSize = 0;
+ }
 
  private static final int DEFAULT_TABLE_SIZE = 101;
 
