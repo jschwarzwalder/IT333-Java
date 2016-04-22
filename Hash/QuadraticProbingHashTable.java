cfb10d0f5cf42af12fae50c82f203583cad330d5
Jami <jamischwarzwalder@gmail.com>
2016-04-21 23:34:40 -0700

    insert routine for hash tables with quadratic probing

--- Hash/QuadraticProbingHashTable.java
+++ Hash/QuadraticProbingHashTable.java
@@ -28,21 +28,49 @@ public class QuadraticProbingHashTable<AnyType>
 	array[ i ] = null;
 	}
 	
+	
+	
 	/**
-	* Internal method to allocate array.
-	* @param arraySize the size of the array.
+	* Find an item in the hash table.
+	* @param x the item to search for.
+	* @return the matching item.
 	*/
-	private void allocateArray( int arraySize )
+	public boolean contains( AnyType x )
 	{
-	array = new HashEntry[ nextPrime( arraySize ) ];
+	int currentPos = findPos( x );
+	return isActive( currentPos );
 	}
 	
-	public boolean contains( AnyType x )
-	{ /* Figure 5.16 */ }
+	
+	 /**
+	* Insert into the hash table. If the item is
+	* already present, do nothing.
+	* @param x the item to insert.
+	*/
 	public void insert( AnyType x )
-	{ /* Figure 5.17 */ }
+	{
+	// Insert x as active
+	int currentPos = findPos( x );
+	if( isActive( currentPos ) )
+	return;
+	
+	array[ currentPos ] = new HashEntry<>( x, true );
+	
+	// Rehash; see Section 5.5
+	if( ++currentSize > array.length / 2 )
+	rehash( );
+	}
+	
+	/**
+	* Remove from the hash table.
+	* @param x the item to remove.
+	*/
 	public void remove( AnyType x )
-	{ /* Figure 5.17 */ }
+	{
+	int currentPos = findPos( x );
+	if( isActive( currentPos ) )
+	array[ currentPos ].isActive = false;
+	}
 	
 	private static class HashEntry<AnyType>
 	{
@@ -61,14 +89,64 @@ public class QuadraticProbingHashTable<AnyType>
 	private HashEntry<AnyType> [ ] array; // The array of elements
 	private int currentSize; // The number of occupied cells
 	
+	/**
+	* Internal method to allocate array.
+	* @param arraySize the size of the array.
+	*/
 	private void allocateArray( int arraySize )
-	{ /* Figure 5.15 */ }
+	{
+	array = new HashEntry[ nextPrime( arraySize ) ];
+	}
+	
+	/**
+	* Return true if currentPos exists and is active.
+	* @param currentPos the result of a call to findPos.
+	* @return true if currentPos is active.
+	*/
 	private boolean isActive( int currentPos )
-	{ /* Figure 5.16 */ }
+	{
+	return array[ currentPos ] != null && array[ currentPos ].isActive;
+	}
+	/**
+	* Method that performs quadratic probing resolution in half-empty table.
+	* @param x the item to search for.
+	* @return the position where the search terminates.
+	*/
 	private int findPos( AnyType x )
-	{ /* Figure 5.16 */ }
+	{
+	int offset = 1;
+	int currentPos = myhash( x );
+	
+	while( array[ currentPos ] != null &&
+	!array[ currentPos ].element.equals( x ) )
+	{
+	currentPos += offset; // Compute ith probe
+	offset += 2;
+	if( currentPos >= array.length )
+	currentPos -= array.length;
+	}
+	
+	return currentPos;
+	}
+	
+	
 	private void rehash( )
-	{ /* Figure 5.22 */ }
+	/**
+	* Rehashing for quadratic probing hash table.
+	*/
+	private void rehash( )
+	{
+	HashEntry<AnyType> [ ] oldArray = array;
+	
+	// Create a new double-sized, empty table
+	allocateArray( nextPrime( 2 * oldArray.length ) );
+	currentSize = 0;
+	
+	// Copy table over
+	for( int i = 0; i < oldArray.length; i++ )
+	if( oldArray[ i ] != null && oldArray[ i ].isActive )
+	insert( oldArray[ i ].element );
+	}
 	
 	private int myhash( AnyType x )
 	{ /* See online code */ }
