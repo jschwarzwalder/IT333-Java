3c96e9156624dbee0eadaf4e7efb209adc9cec71
Jami <jamischwarzwalder@gmail.com>
2016-04-26 00:11:20 -0700

    trying to calculate hash

--- JamiSchwarzwalder-ExerciseHashTables/src/edu/greenriver/it/address/Address.java
+++ JamiSchwarzwalder-ExerciseHashTables/src/edu/greenriver/it/address/Address.java
@@ -1,28 +1,31 @@
 package edu.greenriver.it.address;
 
 public class Address {
-	private int number;
-    private String street;
-    private String city;
-    private String state;
-    private int zip;
+	private static int number;
+    private static String street;
+    private static String city;
+    private static String state;
+    private static int zip;
 	    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
-   
-
-    //constructors, getters, setters, toString()...
+   //constructors, getters, setters, toString()...
     
     public static boolean equals(){
 		return false;
     }
     
     public static int  hashcode(){
-    	
-		return number;
+    	int hash = this.number + this.zip;
+    	hash += this.street.hash();
+    	hash += this.city.hash();
+    	hash += this.state.hash();
+		
+    			
+    	return hash;
     }
     
 
