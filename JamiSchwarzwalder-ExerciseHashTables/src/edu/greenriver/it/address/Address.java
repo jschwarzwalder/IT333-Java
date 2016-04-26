2be512c79a90fa9bd12f5df32f1b6015e5f1739b
Jami <jamischwarzwalder@gmail.com>
2016-04-26 00:08:53 -0700

    Part 3 write custom hashcode

---
+++ JamiSchwarzwalder-ExerciseHashTables/src/edu/greenriver/it/address/Address.java
@@ -0,0 +1,29 @@
+package edu.greenriver.it.address;
+
+public class Address {
+	private int number;
+    private String street;
+    private String city;
+    private String state;
+    private int zip;
+	    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+   
+
+    //constructors, getters, setters, toString()...
+    
+    public static boolean equals(){
+		return false;
+    }
+    
+    public static int  hashcode(){
+    	
+		return number;
+    }
+    
+
+}
