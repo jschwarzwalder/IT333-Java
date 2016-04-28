86a052cdb3f76b9706cd5f23cc536b3a7e3cbbe4
Jami <jamischwarzwalder@gmail.com>
2016-04-27 17:03:12 -0700

    adding Java doc

--- JamiSchwarzwalder-ExerciseHashTables/src/edu/greenriver/it/hashset/HashSet.java
+++ JamiSchwarzwalder-ExerciseHashTables/src/edu/greenriver/it/hashset/HashSet.java
@@ -1,5 +1,16 @@
 package edu.greenriver.it.hashset;
 
+/*
+ * Jami Schwarzwalder
+ * 4/26/2016
+ * HashSet.java
+ * Exercises - Hash Tables
+ * 
+ *  For the following class, write a custom hashcode() and equals() method. 
+ *  Both should be based on the fields of the class.
+ * 
+ */
+
 public class HashSet {
 
 	public HashSet() {
