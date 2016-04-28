6c074a9622b57ca067e374bc57764a3fbdf5cef3
Jami <jamischwarzwalder@gmail.com>
2016-04-27 17:30:15 -0700

    added add method

--- JamiSchwarzwalder-ExerciseHashTables/src/edu/greenriver/it/hashset/HashSet.java
+++ JamiSchwarzwalder-ExerciseHashTables/src/edu/greenriver/it/hashset/HashSet.java
@@ -6,20 +6,59 @@ package edu.greenriver.it.hashset;
  * HashSet.java
  * Exercises - Hash Tables
  * 
- *  For the following class, write a custom hashcode() and equals() method. 
- *  Both should be based on the fields of the class.
+ *  Write a class called HashSet that stores an internal hash table of Objects and uses linear probing to address collisions. 
+ *  Assume your hash table is never resized. 
+ *  If a new element is added to a full table, then throw a new IllegalStateException. 
+ *  You class should have the following methods:
+ *  
+ *  a)   add(x)
+ *  b)   contains(x)
+ *  c)   size()
+ *  d)   isEmpty()
+ *  
+ *  5b. Add the remove(x) method to your HashSet class above. 
+ *  This will require you to support a placeholder value in your hash table for removed elements.
+ 
  * 
  */
 
-public class HashSet {
-
-	public HashSet() {
-		// TODO Auto-generated constructor stub
+public class HashSet <AnyType> {
+	//Build a table to store values
+	private Object[] hashTable = new Object[53];
+	
+	//store values for filled and length
+	private int filled = 0;
+	private int length = 53;
+	
+	public int getFilled() {
+		return filled;
 	}
 
+	public int getLength() {
+		return length;
+	}
+			
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
+	}
+	
+	public boolean add(AnyType value){
+		if (filled == length){
+			return false;
+		} else if (value ==null) {
+			return false;
+		} else {
+			int hash = value.hashCode();
+			int hashIndex = hash%length;
+			while (hashTable[hashIndex] != null){
+				hashIndex = (hashIndex + 1) % length;
+			}
+			hashTable[hashIndex] = value;
+			filled ++;
+			return true;
+		}
 	}
 
 }
