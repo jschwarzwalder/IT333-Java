d1c5736f0cea43bdbfe6a906ca5824ce9d940a3d
Jami <jamischwarzwalder@gmail.com>
2016-04-27 17:02:52 -0700

    Building HashSet for problem 5

---
+++ JamiSchwarzwalder-ExerciseHashTables/src/edu/greenriver/it/hashset/HashSet.java
@@ -0,0 +1,14 @@
+package edu.greenriver.it.hashset;
+
+public class HashSet {
+
+	public HashSet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
