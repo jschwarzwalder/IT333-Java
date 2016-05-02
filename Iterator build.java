d7530e35961e681e29ee612f99589f5e6fa670b4
Jami <jamischwarzwalder@gmail.com>
2016-05-01 23:21:29 -0700

    Building methods for midterm study

---
+++ Iterator build.java
@@ -0,0 +1,27 @@
+public class Color
+{
+    //fields
+    private int red;
+    private int green;
+    private int blue;
+    private int alpha;
+
+    //constructors…
+
+    //methods…
+
+    //here is an inner class
+    public class ColorIterator {
+	
+If (red != null) {
+	System.out.println (“red: “ + red);
+}
+If (green != null) {
+	System.out.println (“green: “ + green);
+}
+If (blue != null) {
+	System.out.println (“blue: “ + blue);
+}
+If (alpha != null) {
+	System.out.println (“alpha: “ + alpha);
+}
---
+++ Iterator.java
@@ -0,0 +1,7 @@
+Iterator<Object> it = = myWords.iterator();
+
+while(it.hasNext()) {
+String word = (String)it.next();
+System.out.println(word);
+}
+
