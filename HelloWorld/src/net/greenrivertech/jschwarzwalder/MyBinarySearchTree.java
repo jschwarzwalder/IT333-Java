eba8920612e1ace21bafbe23195faddd23d16495
Jami <jamischwarzwalder@gmail.com>
2016-05-08 02:30:56 -0700

    adding remove() method

--- HelloWorld/src/net/greenrivertech/jschwarzwalder/MyBinarySearchTree.java
+++ HelloWorld/src/net/greenrivertech/jschwarzwalder/MyBinarySearchTree.java
@@ -137,8 +137,33 @@ public class MyBinarySearchTree<AnyType extends Comparable<? super AnyType>> {
 		return t;
 	}
 
+	/**
+	 * Internal method to remove from a subtree.
+	 * 
+	 * @param x
+	 *            the item to remove.
+	 * @param t
+	 *            the node that roots the subtree.
+	 * @return the new root of the subtree.
+	 */
 	private BinaryNode<AnyType> remove(AnyType x, BinaryNode<AnyType> t) {
-		/* Figure 4.25 */ }
+		if (t == null)
+			return t; // Item not found; do nothing
+
+		int compareResult = x.compareTo(t.element);
+
+		if (compareResult < 0)
+			t.left = remove(x, t.left);
+		else if (compareResult > 0)
+			t.right = remove(x, t.right);
+		else if (t.left != null && t.right != null) // Two children
+		{
+			t.element = findMin(t.right).element;
+			t.right = remove(t.element, t.right);
+		} else
+			t = (t.left != null) ? t.left : t.right;
+		return t;
+	}
 
 	private void printTree(BinaryNode<AnyType> t) {
 		/* Figure 4.56 */ }
