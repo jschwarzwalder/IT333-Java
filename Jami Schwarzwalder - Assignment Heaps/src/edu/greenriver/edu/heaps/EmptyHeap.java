0dd92ffafcb39e6e141906f47adb6424798103d6
Jami <jamischwarzwalder@gmail.com>
2016-05-17 00:09:53 -0700

    percolateUp method written

--- Jami Schwarzwalder - Assignment Heaps/src/edu/greenriver/edu/heaps/EmptyHeap.java
+++ Jami Schwarzwalder - Assignment Heaps/src/edu/greenriver/edu/heaps/EmptyHeap.java
@@ -22,7 +22,7 @@ import java.util.Queue;
  * 
  */
 
-public class EmptyHeap<T> implements Queue<T> {
+public class EmptyHeap<T extends Comparable<T>> implements Queue<T> {
 	private ArrayList<T> heapArray = new ArrayList<T> ();
 	private int heapSize;
 	
@@ -37,11 +37,57 @@ public class EmptyHeap<T> implements Queue<T> {
 		heapSize ++;
 		return true;
 	} else {
-		
+		heapArray.add(newElement);
+		heapSize ++;
+		percolateUp();
 	}
+	
+	
 		
 	}
 
+	/**
+	 * Internal method to percolate up in the heap.
+	 * 
+	 * @param childIndex
+	 *            the index at which the percolate begins.
+	 */
+	private void percolateUp(int childIndex){
+		int parentIndex = (childIndex +1)/3;
+		if (childIndex <= 1){
+			return;
+		}
+		if(heapArray.get(parentIndex).compareTo(heapArray.get(childIndex)) > 0){
+			T temp = heapArray.get(childIndex);
+			heapArray.set(childIndex, heapArray.get(parentIndex) );
+			heapArray.set(parentIndex, temp);
+			percolateUp(parentIndex);
+				
+		}
+	}
+	
+	/**
+	 * Internal method to percolate down in the heap.
+	 * 
+	 * @param hole
+	 *            the index at which the percolate begins.
+	 */
+	private void percolateDown(int hole) {
+		int child;
+		T tmp = heapArray.get(hole);
+
+		for (; hole * 2 <= heapSize; hole = child) {
+			child = hole * 2;
+			if (child != heapSize && heapArray.get(child + 1).compareTo(heapArray.get(child)) < 0)
+				child++;
+			if (heapArray.get(child).compareTo(tmp) < 0)
+				heapArray.set(hole, heapArray.get(child));
+			else
+				break;
+		}
+		heapArray.get(tmp);
+	}
+	
 	@Override
 	public T peek() // findMin() operation
 	{
@@ -56,12 +102,12 @@ public class EmptyHeap<T> implements Queue<T> {
 
 	@Override
 	public int size() {
-		return 0;
+		return heapSize;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return false;
+		return heapSize == 0;
 	}
 
 	@Override
