acb29cdb70eef6e9637ab403a1fc6ed6758e0bd1
Jami <jamischwarzwalder@gmail.com>
2016-05-18 23:37:51 -0700

    Testing Heap remove method

--- Jami Schwarzwalder - Assignment Heaps/src/edu/greenriver/edu/heaps/EmptyHeap.java
+++ Jami Schwarzwalder - Assignment Heaps/src/edu/greenriver/edu/heaps/EmptyHeap.java
@@ -32,77 +32,90 @@ public class EmptyHeap<T extends Comparable<T>> implements Queue<T> {
 	public static void main(String[] args) {
 		EmptyHeap<String> heap = new EmptyHeap<String>();
 		heap.add("F");
-		//System.out.println(heap);
+		// System.out.println(heap);
 		heap.percolateUp(1);
 		heap.add("G");
 		heap.add("H");
-		//System.out.println(heap);
-		heap.add("D"); //this should move up via percolateUp function
-		//System.out.println(heap);
-		heap.add("A");//this should move up via percolateUp function
-		//System.out.println(heap);
+		// System.out.println(heap);
+		heap.add("D"); // this should move up via percolateUp function
+		// System.out.println(heap);
+		heap.add("A");// this should move up via percolateUp function
+		// System.out.println(heap);
 		heap.add("C");
-		//System.out.println(heap);
+		// System.out.println(heap);
 		heap.add("B");
 		System.out.println(heap);
 		heap.remove();
 		System.out.println(heap);
-		
-		
+		while (!heap.isEmpty()){
+			//System.out.println(heap.isEmpty());
+			System.out.print(heap.remove() + " size: ");
+			System.out.print( heap.size() + ", ");
+		}
+
 		EmptyHeap<Integer> numHeap = new EmptyHeap<Integer>();
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(15);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(14);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(13);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(12);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(11);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(10);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(9);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(8);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(7);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(6);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(5);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(4);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(3);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(2);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(1);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
 		numHeap.add(0);
-		//System.out.println(numHeap);
+		// System.out.println(numHeap);
+		System.out.println();
+		while (!numHeap.isEmpty()){
+			//System.out.println(numHeap.isEmpty());
+
+			System.out.print(numHeap.remove() + ", ");
+			//System.out.print( numHeap.size() + );
+
+		}
+
 	}
 
 	private ArrayList<T> heapArray = new ArrayList<T>();
+
 	private int heapSize;
 
 	public EmptyHeap() {
-
+		heapArray.add(null);
 	}
 
 	@Override
 	public boolean add(T newElement) // insert() operation
 	{
 		if (heapSize <= 0) {
-			heapArray.add(null);
 			heapArray.add(1, newElement);
 			heapSize++;
 			return true;
 		} else {
 			boolean results = heapArray.add(newElement);
-			if (results != true){
+			if (results != true) {
 				return false;
 			}
 			heapSize++;
@@ -132,12 +145,15 @@ public class EmptyHeap<T extends Comparable<T>> implements Queue<T> {
 		}
 	}
 
-
-
 	@Override
-	public T peek() // findMin() operation
-	{
-		return heapArray.get(1);
+	public T peek() { // findMin() operation
+		if (heapSize > 0) {
+			return heapArray.get(1);
+		} else {
+			return null;
+		}
+		
+
 	}
 
 	@Override
@@ -146,12 +162,16 @@ public class EmptyHeap<T extends Comparable<T>> implements Queue<T> {
 		T removedNode = peek();
 		T lastNode = heapArray.get(heapSize);
 		heapArray.remove(heapSize);
-		heapArray.set(1, lastNode);
-		heapSize --;
-		percolateDown(1);
+		if (heapSize > 1) {
+			heapArray.set(1, lastNode);
+			heapSize--;
+			percolateDown(1);
+		} else {
+			heapSize--;
+		}
 		return removedNode;
 	}
-	
+
 	/**
 	 * Internal method to percolate down in the heap.
 	 * 
@@ -159,44 +179,71 @@ public class EmptyHeap<T extends Comparable<T>> implements Queue<T> {
 	 *            the index at which the percolate begins.
 	 */
 	private void percolateDown(int parentIndex) {
-		
+
 		if (parentIndex < 1 || parentIndex > heapSize) {
 			throw new IndexOutOfBoundsException("Error, Index is not valid for this heap. You entered " + parentIndex);
 		}
-		
-		int childLeftIndex = (3 * parentIndex ) - 1;
+
+		int childLeftIndex = (3 * parentIndex) - 1;
 		int childMiddleIndex = 3 * parentIndex;
-		int childRightIndex = (3 * parentIndex ) + 1;
-		int smallestChildIndex;
-		
-		T childLeft = heapArray.get(childLeftIndex);
-		T childMiddle = heapArray.get(childMiddleIndex);
-		T childRight = heapArray.get(childRightIndex);
+		int childRightIndex = (3 * parentIndex) + 1;
+		int smallestChildIndex = -1;
+
 		T parentValue = heapArray.get(parentIndex);
-		
-		if (childLeft.compareTo(childMiddle) < 0){ //childLeft is smaller
-			if (childLeft.compareTo(childRight) < 0){ //childLeft is smallest
-				smallestChildIndex = childLeftIndex;
-			} else { //childRight is smallest
+		T childLeft = null ;
+		T childMiddle = null;
+
+		if (childLeftIndex <= heapSize) {
+			childLeft = heapArray.get(childLeftIndex);
+		} else {
+			//if no children
+			return ;
+		}
+		if (childMiddleIndex <= heapSize) {
+			childMiddle = heapArray.get(childMiddleIndex);
+
+		} else {
+			//if childMiddle is null than child Left is smallest
+			smallestChildIndex = childLeftIndex;
+			
+		}
+
+		if (childRightIndex <= heapSize) {
+			// if node has 3 children
+			T childRight = heapArray.get(childRightIndex);
+
+			if (childLeft.compareTo(childMiddle) < 0) { // childLeft is smaller
+				if (childLeft.compareTo(childRight) < 0) { // childLeft is smallest
+					smallestChildIndex = childLeftIndex;
+				} else { // childRight is smallest
+					smallestChildIndex = childRightIndex;
+				}
+			} else if (childMiddle.compareTo(childRight) < 0) { // childMiddle is smallest
+				smallestChildIndex = childMiddleIndex;
+			} else { // childRight is smallest
 				smallestChildIndex = childRightIndex;
 			}
-		} else if (childMiddle.compareTo(childRight) < 0){ //childMiddle is smallest
-			smallestChildIndex = childMiddleIndex;
-		} else { //childRight is smallest
-			smallestChildIndex = childRightIndex;
+
+		} else if (childMiddleIndex <= heapSize){
+			//if childRight is null than just compare middle and left 
+			if (childLeft.compareTo(childMiddle) < 0) { // childLeft is smaller
+				smallestChildIndex = childLeftIndex;
+			} else { // childRight is smallest
+				smallestChildIndex = childMiddleIndex;
+			}
 		}
 		
-		if (parentValue.compareTo(heapArray.get(smallestChildIndex)) > 0 ){ //parent is larger than smallest child
-			
+		
+
+		if (parentValue.compareTo(heapArray.get(smallestChildIndex)) > 0) { // parent  is larger than smallest child
+
 			heapArray.set(parentIndex, heapArray.get(smallestChildIndex));
 			heapArray.set(smallestChildIndex, parentValue);
-			
+
 			percolateDown(smallestChildIndex);
 
 		}
-		
-		
-		
+
 	}
 
 	@Override
@@ -212,9 +259,10 @@ public class EmptyHeap<T extends Comparable<T>> implements Queue<T> {
 	@Override
 	public void clear() {
 		heapArray.clear();
+		heapArray.add(null);
 		heapSize = 0;
 	}
-	
+
 	public String toString() {
 		return heapArray.toString();
 	}
